59ff6592f126faea471c3ef3cc58b68ed5ec1d84
Segz <segz@Segzs-MacBook-Pro.local>
2024-01-22 10:56:39 +0100

    new

--- app/Services/MyMenu.php
+++ app/Services/MyMenu.php
@@ -171,89 +171,89 @@ class MyMenu implements SidebarInterface
                     ],
                 ],
             ],
-            // [
-            //     'type' => 'breaker',
-            //     'name' => 'DEV TOOLS',
-            //     'description' => 'Development Environment',
-            // ],
-            // [
-            //     'type' => 'menu',
-            //     'name' => 'Builder',
-            //     'conditions' => [
-            //         [
-            //             'type' => 'or',
-            //             'condition' => env('APP_ENV') == 'local'
-            //         ],
-            //     ],
-            //     'children' => [
-            //         [
-            //             'type' => 'submenu',
-            //             'name' => 'Form Builder 1',
-            //             'link' => 'http://admin.pixelstrap.com/cuba/theme/form-builder-1.html',
-            //         ],
-            //         [
-            //             'type' => 'submenu',
-            //             'name' => 'Form Builder 2',
-            //             'link' => 'http://admin.pixelstrap.com/cuba/theme/form-builder-2.html',
-            //         ],
-            //         [
-            //             'type' => 'submenu',
-            //             'name' => 'Page Builder',
-            //             'link' => 'http://admin.pixelstrap.com/cuba/theme/pagebuild.html',
-            //         ],
-            //         [
-            //             'type' => 'submenu',
-            //             'name' => 'Buttom Builder',
-            //             'link' => 'http://admin.pixelstrap.com/cuba/theme/button-builder.html',
-            //         ],
-            //     ]
-            // ],
-            // [
-            //     'type' => 'menu',
-            //     'name' => 'Documentation',
-            //     'conditions' => [
-            //         [
-            //             'type' => 'or',
-            //             'condition' => env('APP_ENV') == 'local'
-            //         ],
-            //     ],
-            //     'children' => [
-            //         [
-            //             'type' => 'submenu',
-            //             'name' => 'Frontend Docs',
-            //             'link' => 'https://docs.pixelstrap.com/cuba/all_in_one/document/index.html',
-            //         ],
-            //         [
-            //             'type' => 'submenu',
-            //             'name' => 'Adminetic Docs',
-            //             'link' => 'https://pratikdai404.gitbook.io/adminetic/',
-            //         ],
-            //     ]
-            // ],
-            // [
-            //     'type' => 'link',
-            //     'name' => 'Github',
-            //     'icon' => 'fab fa-github',
-            //     'link' => 'https://github.com/pratiksh404/adminetic',
-            //     'conditions' => [
-            //         [
-            //             'type' => 'or',
-            //             'condition' => env('APP_ENV') == 'local'
-            //         ],
-            //     ],
-            // ],
-            // [
-            //     'type' => 'link',
-            //     'name' => 'Font Awesome',
-            //     'icon' => 'fa fa-font',
-            //     'link' => route('fontawesome'),
-            //     'conditions' => [
-            //         [
-            //             'type' => 'or',
-            //             'condition' => env('APP_ENV') == 'local'
-            //         ],
-            //     ],
-            // ],
+            [
+                'type' => 'breaker',
+                'name' => 'DEV TOOLS',
+                'description' => 'Development Environment',
+            ],
+            [
+                'type' => 'menu',
+                'name' => 'Builder',
+                'conditions' => [
+                    [
+                        'type' => 'or',
+                        'condition' => env('APP_ENV') == 'local'
+                    ],
+                ],
+                'children' => [
+                    [
+                        'type' => 'submenu',
+                        'name' => 'Form Builder 1',
+                        'link' => 'http://admin.pixelstrap.com/cuba/theme/form-builder-1.html',
+                    ],
+                    [
+                        'type' => 'submenu',
+                        'name' => 'Form Builder 2',
+                        'link' => 'http://admin.pixelstrap.com/cuba/theme/form-builder-2.html',
+                    ],
+                    [
+                        'type' => 'submenu',
+                        'name' => 'Page Builder',
+                        'link' => 'http://admin.pixelstrap.com/cuba/theme/pagebuild.html',
+                    ],
+                    [
+                        'type' => 'submenu',
+                        'name' => 'Buttom Builder',
+                        'link' => 'http://admin.pixelstrap.com/cuba/theme/button-builder.html',
+                    ],
+                ]
+            ],
+            [
+                'type' => 'menu',
+                'name' => 'Documentation',
+                'conditions' => [
+                    [
+                        'type' => 'or',
+                        'condition' => env('APP_ENV') == 'local'
+                    ],
+                ],
+                'children' => [
+                    [
+                        'type' => 'submenu',
+                        'name' => 'Frontend Docs',
+                        'link' => 'https://docs.pixelstrap.com/cuba/all_in_one/document/index.html',
+                    ],
+                    [
+                        'type' => 'submenu',
+                        'name' => 'Adminetic Docs',
+                        'link' => 'https://pratikdai404.gitbook.io/adminetic/',
+                    ],
+                ]
+            ],
+            [
+                'type' => 'link',
+                'name' => 'Github',
+                'icon' => 'fab fa-github',
+                'link' => 'https://github.com/pratiksh404/adminetic',
+                'conditions' => [
+                    [
+                        'type' => 'or',
+                        'condition' => env('APP_ENV') == 'local'
+                    ],
+                ],
+            ],
+            [
+                'type' => 'link',
+                'name' => 'Font Awesome',
+                'icon' => 'fa fa-font',
+                'link' => route('fontawesome'),
+                'conditions' => [
+                    [
+                        'type' => 'or',
+                        'condition' => env('APP_ENV') == 'local'
+                    ],
+                ],
+            ],
         ];
     }
 }
